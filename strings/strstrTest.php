50de6e348c6c6f2fe11d0b5870008a407b37fa60
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-08-08 13:12:45 -0300

    commit inicial

---
+++ strings/strstrTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * strstr
+ *
+ * Encontra a primeira ocorrencia de uma string
+ *
+ * http://php.net/manual/pt_BR/function.strstr.php
+ */
+class strstrTest extends \PHPUnit\Framework\TestCase {
+
+    /**
+     * string strstr ( string $haystack , mixed $needle [, bool $before_needle ] )
+     *
+     * Retorna parte da string haystack a partir da primeira ocorrência de needle até o final de haystack.
+     */
+//    function setUp () {
+//        $this->test = [
+//            [
+//                'resultado' => '@example.com',
+//                'haystack'  => 'name@example.com',
+//                'needle'    => '@'
+//            ],
+//            ['resultado' => '.com', 'haystack' => 'name@example.com', 'needle' => '.'],
+//        ];
+//    }
+//
+//    function testStrstr() {
+//        foreach ($this->test as $test) {
+//            $this->assertEquals($test['resultado'], strstr($test['haystack'], $test['needle']));
+//        }
+//    }
+    function testStrstr() {
+        $this->assertEquals('@example.com', strstr('name@example.com', '@'));
+
+        $this->assertEquals('.com', strstr('name@example.com', '.'));
+
+        $this->assertEquals('-456-789-', strstr('123-456-789-', '-'));
+
+        $this->assertEquals('.999', strstr('9.999', '.'));
+    }
+
+}
