6ca236aa06dd51fbdcdc5ef69b3b4d9ac133bf58
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-05 22:49:47 -0300

    test_current_next_prev_end

--- arrays/arraysTest.php
+++ arrays/arraysTest.php
@@ -102,5 +102,31 @@ class arraysTest extends \PHPUnit\Framework\TestCase {
         $this->assertEquals([2 => "c", 3 => "d"],      array_slice($input, 2, 2, true));
         
     }
+    
+    /**
+     * current — Return the current element in an array
+     * http://php.net/manual/en/function.current.php
+     * 
+     * next — Advance the internal pointer of an array
+     * http://php.net/manual/en/function.next.php
+     * 
+     * prev — Rewind the internal array pointer
+     * http://php.net/manual/en/function.prev.php
+     * 
+     * end — Set the internal pointer of an array to its last element
+     * http://php.net/manual/en/function.end.php
+     * 
+     */
+    function test_current_next_prev_end() {
+
+        $transport = array('foot', 'bike', 'car', 'plane');
+        
+        $this->assertEquals('foot',  current($transport));
+        $this->assertEquals('bike',  next($transport));
+        $this->assertEquals('car',   next($transport));
+        $this->assertEquals('bike',  prev($transport));
+        $this->assertEquals('plane', end($transport));
+        
+    }
 
 }
