aa2c0e9f7c7e615c548e8fa7acbb10c26febb7a2
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-05 22:56:51 -0300

    test_addslashes_stripslashes

--- strings/stringsTest.php
+++ strings/stringsTest.php
@@ -63,5 +63,20 @@ class stringsTest extends \PHPUnit\Framework\TestCase {
         $this->assertEquals('yz',    substr($string, 23));
         $this->assertEquals('z',     substr($string, 24));
     }
+    
+    /**
+     * addslashes — Quote string with slashes
+     * http://php.net/manual/en/function.addslashes.php
+     * 
+     * stripslashes — Un-quotes a quoted string
+     * http://php.net/manual/en/function.stripslashes.php
+     */
+    function test_addslashes_stripslashes() {
+        
+        $this->assertEquals("Is your name O\'reilly?", addslashes("Is your name O'reilly?"));
+        
+        $this->assertEquals("Is your name O'reilly?", stripslashes("Is your name O\'reilly?"));
+        
+    }    
 
 }
