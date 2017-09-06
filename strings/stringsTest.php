4228bca2b140d9abff23179075f76afddc1ef79a
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-06 10:54:20 -0300

    strlen

--- strings/stringsTest.php
+++ strings/stringsTest.php
@@ -134,6 +134,21 @@ class stringsTest extends \PHPUnit\Framework\TestCase {
         
         $this->assertEquals(['abc', 'def', 'ghi'], str_split("abcdefghi", 3));
     }
+    
+    /**
+     * strlen — Get string length
+     * 
+     * int strlen ( string $string )
+     * 
+     * http://php.net/manual/en/function.strlen.php
+     */
+    function test_strlen() {
+        $this->assertEquals(3, strlen("abc"));
+        $this->assertEquals(5, strlen("a b c"));
+        $this->assertEquals(1, strlen(" "));
+        $this->assertEquals(0, strlen(""));
+        $this->assertEquals(0, strlen(null));
+    }
 
 
 
