859c0ee57cf28e64b574c3b7a0927cbf29986382
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-12 09:25:29 -0300

    PropertyExistsTest

---
+++ classes/property_exists_Test.php
@@ -0,0 +1,42 @@
+<?php
+
+#
+#  mock
+#
+class MyClass {
+    public $var;
+    protected $bar;
+    private $die;
+    static $lish;
+}
+
+#
+# test
+#
+class PropertyExistsTest extends \PHPUnit\Framework\TestCase {
+
+    /**
+     * property_exists — Checks if the object or class has a property
+     *
+     * bool property_exists ( mixed $class , string $property )
+     *
+     * http://php.net/manual/en/function.property-exists.php
+     */
+    function test_property_exists() {
+        $this->assertTrue(property_exists('MyClass', 'var'));
+        $this->assertFalse(property_exists('Foo', 'watch'));
+    }
+
+    function test_property_exists_for_protected() {
+        $this->assertTrue(property_exists('MyClass', 'bar'));
+    }
+
+    function test_property_exists_for_private() {
+        $this->assertTrue(property_exists('MyClass', 'die'));
+    }
+
+    function test_property_exists_for_static() {
+        $this->assertTrue(property_exists('MyClass', 'lish'));
+    }
+
+}
